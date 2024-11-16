9b605427d06c7ed1573203d513d0d94deb4f163c
AndrewW16R <andrew12rw@gmail.com>
2024-11-15 18:26:11 -0600

    Initial Unity Project Commit
    Includes scene set-up and player set-up

---
+++ Assets/Game/Scripts/PlayerMovement.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+
+    //left/right input
+    float dirX;
+
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float jumpStrength;
+    [SerializeField] private float doubleJumpStrength;
+
+    [SerializeField] private LayerMask jumpableGround;
+
+    //How many jumps the player can do before needing to be grounded again
+    [SerializeField] public int jumpsAvailable = 2;
+    //Player is assigned the number of Jumps available from frame 1, used to determine how many jumps the player gets when jumps refresh
+    public int maxJumps;
+    //Has the player used their first jump?
+    private bool initialJumpUsed = false;
+
+    //The initial intensity of gravity which acts upon the player character
+    private float initialGravity;
+    //Is the process of fast falling currently taking place?
+    public bool fastFalling = false;
+    //How much the default gravity is multiplied by within the process of fast falling
+    [SerializeField] public float fastfallGravMultiplier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+
+        maxJumps = jumpsAvailable;
+        initialGravity = rb.gravityScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Walking movement
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * movementSpeed, rb.velocity.y);
+
+        //Checks for jump input and executes jump in under proper conditions
+        UpdateJump();
+    }
+
+    private void UpdateJump()
+    {
+        //Checks if player is grounded
+        if (IsGrounded())
+        {
+            rb.gravityScale = initialGravity;
+            fastFalling = false;
+            jumpsAvailable = maxJumps;
+            initialJumpUsed = false;
+        }
+
+        //jumping
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpStrength);
+            jumpsAvailable = jumpsAvailable - 1;
+            initialJumpUsed = true;
+
+        }//if player is already not grounded before the intial jump is used up, both the intial jump and the first addition jump are used up.
+        else if (Input.GetButtonDown("Jump") && initialJumpUsed == false && jumpsAvailable > 0 && !IsGrounded())
+        {
+            rb.gravityScale = initialGravity;
+            fastFalling = false;
+            rb.velocity = new Vector2(rb.velocity.x, doubleJumpStrength);
+            jumpsAvailable = jumpsAvailable - 2;
+            initialJumpUsed = true;
+
+        } //When double jump is used
+        else if (Input.GetButtonDown("Jump") && jumpsAvailable > 0 && !IsGrounded())
+        {
+            rb.gravityScale = initialGravity;
+            fastFalling = false;
+            rb.velocity = new Vector2(rb.velocity.x, doubleJumpStrength);
+            jumpsAvailable = jumpsAvailable - 1;
+        }
+
+        if (rb.velocity.y < -.1f && !IsGrounded() && fastFalling == false)
+        {
+            rb.gravityScale = rb.gravityScale * fastfallGravMultiplier;
+            fastFalling = true;
+        }
+    }
+
+   
+   
+
+    public bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+}
